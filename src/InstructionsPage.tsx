6dfa3083126fd88745d6325d7e9049157bc2c08e
softwarebyze <zackebenfeld@gmail.com>
2023-04-04 12:37:59 +0300

    added instructions page to stop signal

---
+++ src/InstructionsPage.tsx
@@ -0,0 +1,16 @@
+function InstructionsPage() {
+  return (
+    <>
+      <h1>Stop Signal</h1>
+      <p>
+        You will see pictures in either a dark blue or light gray border. Touch
+        the picture if you see a blue border. Be sure to touch the screen inside
+        the box outline. Do not touch the screen if you see a gray border. Go as
+        fast as you can but remember accuracy is more important than speed!
+      </p>
+      <button>Start Game</button>
+    </>
+  )
+}
+
+export default InstructionsPage
--- src/StopSignal.tsx
+++ src/StopSignal.tsx
@@ -1,5 +1,12 @@
+import InstructionsPage from './InstructionsPage'
+
 function StopSignal() {
-  return <div>Stop Signal</div>
+  return (
+    <>
+      <div>Stop Signal</div>
+      <InstructionsPage />
+    </>
+  )
 }
 
 export default StopSignal

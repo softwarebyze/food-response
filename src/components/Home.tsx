5a5d889a168fe89ac03ce97722bc45f2868f8469
softwarebyze <zackebenfeld@gmail.com>
2023-04-15 23:50:57 +0300

    changed home page to match Project Health

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,37 @@
-function Home() {
-  return <div>Home</div>
-}
+import { Link } from 'react-router-dom'
+import { TaskInfo } from '../types/Task'
 
-export default Home
+export default function Home({ tasks }: { tasks: TaskInfo[] }) {
+  return (
+    <section className="hero is-primary">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title is-1">Welcome, USERNAME</h1>
+          <div className="columns">
+            {tasks.map(({ name, path, cover }) => (
+              <div key={name} className="column">
+                <div className="card" style={{ borderRadius: '1em' }}>
+                  <div className="card-image">
+                    <figure className="image is-4by3">
+                      <Link to={path}>
+                        <img src={cover} alt={name} />
+                      </Link>
+                    </figure>
+                  </div>
+                  <div className="card-content">
+                    <div className="media">
+                      <div className="media-content">
+                        <strong>{name}</strong>
+                      </div>
+                    </div>
+                    <div className="content"></div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}

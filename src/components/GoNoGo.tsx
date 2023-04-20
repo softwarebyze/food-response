64de262c5e59ab746984dc7a276f6301d1b5c2a1
softwarebyze <zackebenfeld@gmail.com>
2023-04-20 14:31:34 +0300

    make it playable

--- src/components/GoNoGo.tsx
+++ src/components/GoNoGo.tsx
@@ -1,3 +1,4 @@
+import { images } from '../data/images.json'
 import {
   GoNoGoBorderStyle,
   GoNoGoCue,
@@ -38,22 +39,33 @@ function getRandomSide() {
   return Math.random() < 0.5 ? 'left' : 'right'
 }
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { tasks } from '../data/tasks.json'
 const stages = tasks[1].stages
+const slowdown = 1
+const times = {
+  cue: 1250 * slowdown,
+  interval: 500 * slowdown,
+  error: 500 * slowdown,
+} as const
 
 export default function GoNoGo({ endGame }: { endGame: () => void }) {
-  const src =
-    'https://projecthealthori.org/file/repo/8fafc912bec1c78aaa451e519c9134c2.png'
-  const cue: GoNoGoCue = { side: getRandomSide(), imageType: 'unhealthy' }
-  const { imageType: type, side } = cue
+  const [currentTrialIndex, setCurrentTrialIndex] = useState<number>(0)
   const [gameStage, setGameStage] = useState<GoNoGoGameStage>('cue')
+
+  const { image, border, error, interval } = stages[gameStage]
+  const taskData = images
+  const { src, type } = taskData[currentTrialIndex]
+  // const side = image ? getRandomSide() : null
+  const side = getRandomSide()
+
+  // const src = 'https://projecthealthori.org/file/repo/8fafc912bec1c78aaa451e519c9134c2.png'
+  const cue: GoNoGoCue = { side, imageType: type as ImageType }
   const [response, setResponse] = useState<GoNoGoResponse>({
-    type: null,
+    reaction: null,
     correct: null,
   })
 
-  const { image, border, error, interval } = stages[gameStage]
   const borderStyle = border
     ? getGoNoGoBorderStyle(type as ImageType)
     : 'whiteBorder'
@@ -72,19 +84,71 @@ export default function GoNoGo({ endGame }: { endGame: () => void }) {
     }
   }
 
+  function showInterval() {
+    setGameStage('interval')
+  }
+
+  function showCue() {
+    setGameStage('cue')
+  }
+
+  function showError() {
+    setGameStage('error')
+  }
+
+  function goToNextTrialOrEndGame() {
+    if (currentTrialIndex < taskData.length - 1) {
+      setCurrentTrialIndex((prev) => prev + 1)
+    } else {
+      endGame()
+    }
+  }
+
+  useEffect(() => {
+    if (response.correct === null) return
+    if (response.correct === false) showError()
+    if (response.correct === true) showInterval()
+  }, [response])
+
+  useEffect(() => {
+    showCue()
+  }, [currentTrialIndex])
+
+  function handleReaction(reaction: GoNoGoReaction) {
+    const correct = isGoNoGoResponseCorrect(reaction, cue)
+    setResponse({ reaction, correct })
+  }
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout
+    switch (gameStage) {
+      case 'cue':
+        setResponse({ reaction: null, correct: null })
+        timeout = setTimeout(() => handleReaction('omission'), times.cue)
+        break
+      case 'interval':
+        timeout = setTimeout(goToNextTrialOrEndGame, times.interval)
+        break
+      case 'error':
+        timeout = setTimeout(showInterval, times.error)
+        break
+    }
+    return () => clearTimeout(timeout)
+  }, [gameStage])
+
   return (
     <>
-      {/* {'currentTrialIndex: ' + currentTrialIndex} */}
+      {'currentTrialIndex: ' + currentTrialIndex}
       <br />
-      {/* {'taskData.length: ' + taskData.length} */}
+      {'taskData.length: ' + taskData.length}
       <br />
-      {/* {'slowdown: ' + slowdown + 'x'} */}
+      {'slowdown: ' + slowdown + 'x'}
       <br />
       {'gameStage: ' + gameStage}
       <br />
-      {'response: ' + JSON.stringify(response)}
+      {'side: ' + side}
       <br />
-      <button onClick={next}>Next Stage</button>
+      {'response: ' + JSON.stringify(response)}
       {interval ? (
         <></>
       ) : (
@@ -92,10 +156,20 @@ export default function GoNoGo({ endGame }: { endGame: () => void }) {
           {image && (
             <div className="columns is-mobile">
               <div className="column">
-                {side === 'left' && <img src={src} />}
+                {side === 'left' && (
+                  <img
+                    onClick={() => handleReaction('left-commission')}
+                    src={src}
+                  />
+                )}
               </div>
               <div className="column">
-                {side === 'right' && <img src={src} />}
+                {side === 'right' && (
+                  <img
+                    onClick={() => handleReaction('right-commission')}
+                    src={src}
+                  />
+                )}
               </div>
             </div>
           )}

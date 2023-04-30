a71f4c1e2b832d5987c2558d2356c2966b902ab8
softwarebyze <zackebenfeld@gmail.com>
2023-04-30 21:40:15 +0300

    visualsearch is working well

--- src/components/VisualSearch.tsx
+++ src/components/VisualSearch.tsx
@@ -1,21 +1,143 @@
-import { useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { images } from '../data/images.json'
-import { GameStage } from '../types/Task'
+import { tasks } from '../data/tasks.json'
+import { ImageType, ReactionType } from '../types/Task'
 interface Image {
   src: string
-  type: string
+  type: ImageType
+}
+type VisualSearchGameStage = 'cue' | 'interval' | 'feedback'
+type VisualSearchResponse = {
+  reactionType: null | ReactionType
+  correct: null | boolean
+  responseTime: null | number
+  selectedSrc: null | string
 }
 
-const rawImages: Image[] = images
-const unhealthyImages = rawImages.filter((image) => image.type === 'unhealthy')
-const healthyImage = rawImages.find((image) => image.type === 'healthy')
-const trialImages = [...unhealthyImages, healthyImage].sort(
-  () => Math.random() - 0.5
-)
+const rawImages: Image[] = images as Image[]
+
+function getTrialImages() {
+  const unhealthyImages = rawImages.filter(
+    (image) => image.type === 'unhealthy'
+  )
+  const healthyImages = rawImages.filter((image) => image.type === 'healthy')
+  const healthyImage = healthyImages.sort(() => Math.random() - 0.5)[0]
+  const trialImages = [...unhealthyImages, healthyImage].sort(
+    () => Math.random() - 0.5
+  )
+  return trialImages
+}
+
+function createImageMatrix(numRows: number, numCols: number, images: Image[]) {
+  const imageMatrix = []
+  let imageIndex = 0
+  for (let row = 0; row < numRows; row++) {
+    const imageRow = []
+    for (let col = 0; col < numCols; col++) {
+      imageRow.push(images[imageIndex])
+      imageIndex++
+    }
+    imageMatrix.push(imageRow)
+  }
+  return imageMatrix
+}
+
+const task = tasks[3]
+const { times } = task
 
 export default function VisualSearch({ endGame }: { endGame: () => void }) {
   const [currentTrialIndex, setCurrentTrialIndex] = useState<number>(0)
-  const [gameStage, setGameStage] = useState<GameStage>('init')
+  const trialImages = useMemo(() => getTrialImages(), [currentTrialIndex])
+  const imageMatrix = useMemo(
+    () => createImageMatrix(4, 4, trialImages as Image[]),
+    [currentTrialIndex]
+  )
+  const [gameStage, setGameStage] = useState<VisualSearchGameStage>('interval')
+  const defaultResponse: VisualSearchResponse = {
+    reactionType: null,
+    correct: null,
+    responseTime: null,
+    selectedSrc: null,
+  }
+  const [response, setResponse] =
+    useState<VisualSearchResponse>(defaultResponse)
+  const [cueTimestamp, setCueTimestamp] = useState<number | null>(null)
+
+  function showCue() {
+    setGameStage('cue')
+    setCueTimestamp(Date.now())
+  }
+
+  function showInterval() {
+    setGameStage('interval')
+  }
+
+  useEffect(() => {
+    showInterval()
+  }, [currentTrialIndex])
+
+  const taskData = imageMatrix.flat()
+  function goToNextTrialOrEndGame() {
+    if (currentTrialIndex < taskData.length - 1) {
+      setCurrentTrialIndex((prev) => prev + 1)
+    } else {
+      endGame()
+    }
+  }
+
+  useEffect(() => {
+    let timeout: NodeJS.Timeout
+    switch (gameStage) {
+      case 'cue':
+        setResponse(defaultResponse)
+        timeout = setTimeout(handleOmission, times?.cue)
+        break
+      case 'feedback':
+        timeout = setTimeout(goToNextTrialOrEndGame, times?.interval)
+        break
+      case 'interval':
+        timeout = setTimeout(showCue, times?.interval)
+    }
+    return () => clearTimeout(timeout)
+  }, [gameStage])
+
+  function getBorderStyle(imageSrc: string, type: ImageType) {
+    if (gameStage !== 'feedback') {
+      return ''
+    }
+    if (type === 'healthy') {
+      return 'green-grow'
+    }
+    if (response.correct === false && imageSrc === response.selectedSrc) {
+      return 'red-shrink'
+    }
+  }
+
+  function handleImageClick(imageSrc: string, type: ImageType) {
+    if (gameStage !== 'cue') return
+
+    setResponse({
+      reactionType: 'commission',
+      correct: type === 'healthy',
+      responseTime: cueTimestamp ? Date.now() - cueTimestamp : null,
+      selectedSrc: imageSrc,
+    })
+  }
+
+  function handleOmission() {
+    setResponse({
+      reactionType: 'omission',
+      correct: false,
+      responseTime: null,
+      selectedSrc: null,
+    })
+  }
+  useEffect(() => {
+    if (response.reactionType) {
+      setGameStage('feedback')
+    }
+  }, [response])
+
   return (
     <div>
       <div className="columns is-mobile">
@@ -24,32 +146,22 @@ export default function VisualSearch({ endGame }: { endGame: () => void }) {
             <div className="fixationCross">+</div>
           </div>
         )}
-        <>
-          <div className="column">
-            <img src={trialImages[0]!.src} />
-            <img src={trialImages[1]!.src} />
-            <img src={trialImages[2]!.src} />
-            <img src={trialImages[3]!.src} />
-          </div>
-          <div className="column">
-            <img src={trialImages[4]!.src} />
-            <img src={trialImages[5]!.src} />
-            <img src={trialImages[6]!.src} />
-            <img src={trialImages[7]!.src} />
-          </div>
-          <div className="column">
-            <img src={trialImages[8]!.src} />
-            <img src={trialImages[9]!.src} />
-            <img src={trialImages[10]!.src} />
-            <img src={trialImages[11]!.src} />
-          </div>
-          <div className="column">
-            <img src={trialImages[12]!.src} />
-            <img src={trialImages[13]!.src} />
-            <img src={trialImages[14]!.src} />
-            <img src={trialImages[15]!.src} />
-          </div>
-        </>
+        {['cue', 'feedback'].includes(gameStage) && (
+          <>
+            {imageMatrix.map((row, rowIndex) => (
+              <div key={`row${rowIndex}`} className="column">
+                {row.map((image, colIndex) => (
+                  <img
+                    key={`col${colIndex}`}
+                    className={getBorderStyle(image.src, image.type)}
+                    src={image.src}
+                    onClick={() => handleImageClick(image.src, image.type)}
+                  />
+                ))}
+              </div>
+            ))}
+          </>
+        )}
       </div>
     </div>
   )

10faa173a7aaa898597d5f0dbeb1fcefbe7f5253
softwarebyze <zackebenfeld@gmail.com>
2023-05-03 13:39:40 +0300

    create Break component

---
+++ src/components/Break.tsx
@@ -0,0 +1,14 @@
+export default function Break() {
+  return (
+    <div className="columns">
+      <div className="column is-6 is-offset-3">
+        <div className="card">
+          <div className="card-content">
+            Great job! We're going to take a quick 10 second break before we
+            move on to the next section.
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}

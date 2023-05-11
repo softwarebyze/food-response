a228306f388b6d3983f7a4adf56fcdef32a026ee
softwarebyze <zackebenfeld@gmail.com>
2023-05-11 14:41:01 +0300

    hide the navbar if on login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import 'bulma/css/bulma.min.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
 import Home from './components/Home'
 import LoginPage from './components/LoginPage'
 import Nav from './components/Nav'
@@ -9,9 +9,11 @@ import './main.css'
 import { TaskInfo } from './types/Task'
 
 export default function App() {
+  const location = useLocation()
+  const isLoginPage = location.pathname === '/login'
   return (
     <BrowserRouter>
-      <Nav />
+      {!isLoginPage && <Nav />}
       <Routes>
         <Route path="/" element={<Home tasks={tasks as TaskInfo[]} />} />
         <Route path="/login" element={<LoginPage />} />

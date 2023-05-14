6847dd13bc99f2bb66d0285977b56682cc1cb5c8
softwarebyze <zackebenfeld@gmail.com>
2023-05-14 16:55:05 +0300

    formatting

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,25 +1,33 @@
-import {TaskInfo} from '../types/Task'
+import { TaskInfo } from '../types/Task'
 
 interface ResultsProps {
-    task: TaskInfo
-    endGame: () => void
-    accuracy: number
-    averageResponse: number
+  task: TaskInfo
+  endGame: () => void
+  accuracy: number
+  averageResponse: number
 }
-export default function Results({ endGame, accuracy, averageResponse, }: ResultsProps) {
-    return (
-        <>
-        <div className="box">
+export default function Results({
+  endGame,
+  accuracy,
+  averageResponse,
+}: ResultsProps) {
+  return (
+    <>
+      <div className="box">
         <h1 className="title">Results</h1>
         <ul>
-        <li> Average Response Time: {averageResponse} milliseconds </li>
-        <li> Percent Correct: {accuracy} %</li>
-        <br />
+          <li> Average Response Time: {averageResponse} milliseconds </li>
+          <li> Percent Correct: {accuracy} %</li>
+          <br />
         </ul>
-            <button onClick={endGame} className="button is-info is-large" tabIndex={1}>
-                Resend Data
-            </button>
-        </div>
-        </>
-    )
+        <button
+          onClick={endGame}
+          className="button is-info is-large"
+          tabIndex={1}
+        >
+          Resend Data
+        </button>
+      </div>
+    </>
+  )
 }

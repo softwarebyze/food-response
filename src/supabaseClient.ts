d3e2cb0a149b11f2a68a7520ca70a04418e4acb5
softwarebyze <zackebenfeld@gmail.com>
2023-05-23 21:53:13 +0300

    Change default export to named export

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -2,6 +2,5 @@ import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
 const supabaseUrl: string = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey: string = import.meta.env.VITE_SUPABASE_API_KEY;
-const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);
 
-export default supabase;
+export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey);

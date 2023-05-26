28e3a7e677a239c8e69d3ba3bbbeb7bf918fce04
softwarebyze <zackebenfeld@gmail.com>
2023-05-26 21:49:16 +0300

    stop signal task responses looking good

--- src/utils/recordResponse.ts
+++ src/utils/recordResponse.ts
@@ -1,6 +1,16 @@
 import { supabase } from '../supabaseClient'
-import { ResponseWithTrialData } from '../types/Task'
+import { TaskResponse } from '../types/Task'
 
-export async function recordResponse(response: ResponseWithTrialData) {
-  const { data, error } = await supabase.from('responses').insert(response)
+export async function recordTaskResponse(
+  taskResponseData: Partial<TaskResponse>
+) {
+  const { data, error } = await supabase
+    .from('task_responses')
+    .insert(taskResponseData)
+
+  if (error) {
+    console.error('Error inserting task response:', error)
+  } else {
+    console.log('Task response inserted successfully:', data)
+  }
 }

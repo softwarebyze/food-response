85f69686f1ff236fabb5c6ab1e28be500792b029
softwarebyze <zackebenfeld@gmail.com>
2023-06-06 00:14:47 -0400

    create and wrap App in QueryClient

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { AuthProvider } from './contexts/AuthContext'
 import { UserDataProvider } from './contexts/UserDataContext'
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <AuthProvider>
       <UserDataProvider>
-        <App />
+        <QueryClientProvider client={queryClient}>
+          <App />
+        </QueryClientProvider>
       </UserDataProvider>
     </AuthProvider>
   </React.StrictMode>

9ec694546697690699e7efc5dabab35e785ccd13
softwarebyze <zackebenfeld@gmail.com>
2023-06-06 03:22:14 -0400

    formatting

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { AuthProvider } from './contexts/AuthContext'
 import { UserDataProvider } from './contexts/UserDataContext'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-
 
 const queryClient = new QueryClient()
 

74c999684aa1d82826c9777f0fdc6616be3abedf
softwarebyze <zackebenfeld@gmail.com>
2023-06-07 17:48:49 -0400

    add test to ensure each VisualSearch trial has 1 unique healthy and 15 unique unhealthy foods (currently failing)

--- src/tests/VisualSearch.test.tsx
+++ src/tests/VisualSearch.test.tsx
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import { describe, expect, it } from 'vitest'
 import { prepareTaskData } from '../components/VisualSearch'
 import { tasks } from '../data/tasks.json'
@@ -16,5 +17,19 @@ describe('Visual Search prepareTaskData', () => {
     expect(testTaskData.length).toBe(
       visualSearch.blocks * visualSearch.trialsPerBlock
     )
+  })
+  it('Each trial should have 1 unique healthy food and 15 unique unhealthy foods', () => {
+      testTaskData.forEach((trialImages) => {
+        const healthyImages = trialImages.filter(
+          (trial) => trial.type === 'healthy'
+        )
+        const unhealthyImages = trialImages.filter(
+          (trial) => trial.type === 'unhealthy'
+        )
+        expect(healthyImages.length).toBe(1)
+        expect(_.uniq(healthyImages).length).toBe(1)
+        expect(unhealthyImages.length).toBe(15)
+        expect(_.uniq(unhealthyImages).length).toBe(15)
+      })
   })
 })

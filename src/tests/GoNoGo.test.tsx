4d24dfa5b489cc7060b35c449250341c68a3a976
softwarebyze <zackebenfeld@gmail.com>
2023-06-07 23:28:55 -0400

    test that prepareTaskData gives enough data and doesnt repeat food back-to-back

--- src/tests/GoNoGo.test.tsx
+++ src/tests/GoNoGo.test.tsx
@@ -1,5 +1,29 @@
 import { describe, expect, it } from 'vitest'
-import { getNextStageAfterResponse } from '../components/GoNoGo'
+import {
+  getNextStageAfterResponse,
+  prepareTaskData,
+} from '../components/GoNoGo'
+import { tasks } from '../data/tasks.json'
+import { sampleUserImages } from './sampleUserImages'
+
+describe('GoNoGo prepareTaskData', () => {
+  const goNoGo = tasks[1]
+  const totalTrials = goNoGo.blocks * goNoGo.trialsPerBlock
+  console.log({ totalTrials })
+  const testTaskData = prepareTaskData(sampleUserImages, totalTrials)
+  it(`Should give enough task data for all trials`, () => {
+    expect(testTaskData.length).toBe(totalTrials)
+  })
+  it('Should not repeat any food images back to back', () => {
+    const repeatedFoods = testTaskData.filter(
+      (trial, i) =>
+        i > 0 &&
+        trial.imageType !== 'water' &&
+        trial.src === testTaskData[i - 1].src
+    )
+    expect(repeatedFoods.length).toBe(0)
+  })
+})
 
 describe('get next stage based on response and trial type', () => {
   it('is interval if response is correct', () => {

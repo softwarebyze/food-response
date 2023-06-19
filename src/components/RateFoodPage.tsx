34b9ca5e7e715e72419a533707b7c4f729ace3ea
softwarebyze <zackebenfeld@gmail.com>
2023-06-19 13:13:36 -0400

    updated RateFoodPage to include rating categories

--- src/components/RateFoodPage.tsx
+++ src/components/RateFoodPage.tsx
@@ -1,108 +1,42 @@
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
-import { useEffect, useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
 import { Link } from 'react-router-dom'
-import { useAuth } from '../contexts/AuthContext'
-import { fetchFoodRatings, useUserData } from '../contexts/UserDataContext'
-import { supabase } from '../supabaseClient'
-import { FoodRatingData } from '../types/Task'
+import {
+  HEALTHY_IMAGE_COUNT,
+  UNHEALTHY_IMAGE_COUNT,
+  fetchFoodRatings,
+  useUserData,
+} from '../contexts/UserDataContext'
+import RateCategories from './RateCategories'
+import RateFoods from './RateFoods'
 
 export default function RateFoodPage() {
-  const queryClient = useQueryClient()
-  const [currentRating, setCurrentRating] = useState('')
-  const { session } = useAuth()
-  const { allFoodImages } = useUserData()
+  const { foodCategoryRatings } = useUserData()
   const {
     data: foodRatings,
     isLoading,
     isError,
     isFetching,
   } = useQuery({ queryKey: ['foodRatings'], queryFn: fetchFoodRatings })
-  const { mutate: recordRating } = useMutation({
-    mutationFn: async (foodRating: FoodRatingData) => {
-      return await supabase.from('food_ratings').insert(foodRating)
-    },
-    onSuccess: () => {
-      // Invalidate and refetch
-      queryClient.invalidateQueries({ queryKey: ['foodRatings'] })
-    },
-  })
-  const ratedFoodIds =
-    foodRatings?.map((foodRating) => foodRating.food_id) ?? []
-  const unratedFoods = allFoodImages.filter(
-    (food) => !ratedFoodIds.includes(food.id)
-  )
-  const currentFood = unratedFoods?.length ? unratedFoods[0] : null
 
-  const handleKeyDown = async (event: KeyboardEvent) => {
-    const { key } = event
-    if (!(key >= '1' && key <= '9')) return
-    if (!currentFood) return console.log('no current food')
-    if (!currentFood?.id) return console.log('no current food id')
-    setCurrentRating(key)
-    const keyNumber = parseInt(key)
-    recordRating({
-      rating: keyNumber,
-      food_id: currentFood!.id,
-      user_id: session!.user!.id,
-    })
-    setCurrentRating('')
-  }
-  useEffect(() => {
-    window.addEventListener('keydown', handleKeyDown)
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown)
-    }
-  }, [currentFood])
+  const doneRatingFoods = foodRatings?.length
+    ? foodRatings?.length >= HEALTHY_IMAGE_COUNT + UNHEALTHY_IMAGE_COUNT
+    : false
+
+  const doneRatingCategories = foodCategoryRatings?.length > 0
+
   return (
     <div className="container">
       <h1 className="title">Rate Food Page</h1>
-      {isLoading ? (
-        'loading'
-      ) : isFetching ? (
-        'fetching'
-      ) : !foodRatings ? (
-        'issue loading food ratings'
+      {doneRatingFoods ? (
+        <div>
+          <p>
+            Done rating foods! <Link to="/">Go to Games</Link>
+          </p>
+        </div>
+      ) : doneRatingCategories ? (
+        <RateFoods />
       ) : (
-        <>
-          <h2 className="subtitle">
-            Rated {foodRatings?.length ?? 0} of {allFoodImages?.length} foods
-          </h2>
-          <div className="columns is-centered">
-            {currentFood ? (
-              <div className="column is-narrow is-centered">
-                <div className="is-flex is-justify-content-center">
-                  <img src={currentFood.src} alt="food to rate" />
-                </div>
-                <div className="has-text-centered">
-                  <h1 className="is-size-1">{currentRating}</h1>
-                </div>
-                <div className="is-flex m-3">
-                  {['1', '2', '3', '4', '5', '6', '7', '8', '9'].map((i) => (
-                    <div
-                      key={i}
-                      className="tile button is-outlined p-6 is-align-items-center mr-1"
-                      onClick={() => handleKeyDown({ key: i } as KeyboardEvent)}
-                      onTouchStart={() =>
-                        handleKeyDown({ key: i } as KeyboardEvent)
-                      }
-                    >
-                      <p>{i}</p>
-                    </div>
-                  ))}
-                </div>
-                <p className="has-text-centered">
-                  Rate the food from 1 to 9 with your keyboard
-                </p>
-              </div>
-            ) : (
-              <div>
-                <p>
-                  Done rating foods! <Link to="/">Go to Games</Link>
-                </p>
-              </div>
-            )}
-          </div>
-        </>
+        <RateCategories />
       )}
     </div>
   )

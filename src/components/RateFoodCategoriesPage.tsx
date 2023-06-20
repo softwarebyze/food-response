bb4088dc650bb0123ae99bac2d3a95899c3ef855
softwarebyze <zackebenfeld@gmail.com>
2023-06-19 23:33:18 -0400

    use hook in rate categories page

--- src/components/RateFoodCategoriesPage.tsx
+++ src/components/RateFoodCategoriesPage.tsx
@@ -1,8 +1,13 @@
+import { useFoodCategoryRatings } from '../hooks/useFoodCategoryRatings'
+
 export default function RateFoodCategoriesPage() {
+  const { ratedCategories } = useFoodCategoryRatings()
   return (
     <div className="container">
       <h1 className="title">Rate Food Categories Page</h1>
-      <h2 className="subtitle">Rated x food categories</h2>
+      <h2 className="subtitle">
+        Rated {ratedCategories.length} food categories
+      </h2>
       <div className="columns is-centered"></div>
     </div>
   )

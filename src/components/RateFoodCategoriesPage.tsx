8817a4e03ef21211f5167481a18ace908d5e9746
softwarebyze <zackebenfeld@gmail.com>
2023-06-20 02:10:27 -0400

    added state for food category ratings

--- src/components/RateFoodCategoriesPage.tsx
+++ src/components/RateFoodCategoriesPage.tsx
@@ -1,18 +1,47 @@
+import { useState } from 'react'
 import { allUnhealthyCategories } from '../data/images.ts'
-import { useFoodCategoryRatings } from '../hooks/useFoodCategoryRatings'
 
 export default function RateFoodCategoriesPage() {
-  const { data: foodCategoryRatings } = useFoodCategoryRatings()
+  const [currentFoodCategoryRatings, setCurrentFoodCategoryRatings] = useState(
+    () =>
+      allUnhealthyCategories.map((category) => ({
+        category,
+        rating: 0,
+      }))
+  )
+
+  const handleRatingChange = (category: string) => {
+    setCurrentFoodCategoryRatings((prevFoodCategoryRatings) => {
+      const categoryIndex = prevFoodCategoryRatings.findIndex(
+        ({ category: ratingCategory }) => ratingCategory === category
+      )
+      const currentRating = prevFoodCategoryRatings[categoryIndex].rating
+      const newRating = currentRating === 0 ? 1 : 0
+      const newFoodCategoryRatings = [...prevFoodCategoryRatings]
+      newFoodCategoryRatings[categoryIndex] = {
+        category,
+        rating: newRating,
+      }
+      return newFoodCategoryRatings
+    })
+  }
+
   return (
     <div className="container">
-      <h1 className="title">Rate Food Categories Page</h1>
-      <h2 className="subtitle">
-        Rated {foodCategoryRatings?.length ?? '...'} food categories Please rate
-        at least 4 food categories
-      </h2>
+      <h1 className="title">Choose Food Categories</h1>
+      <h2 className="subtitle">Please choose at least 4 food categories</h2>
       <div className="column">
-        {allUnhealthyCategories.map((category) => (
-          <p key={category}>{category}</p>
+        {currentFoodCategoryRatings.map(({ category, rating }) => (
+          <div key={category}>
+            <label>
+              {category}
+              <input
+                type="checkbox"
+                checked={Boolean(rating)}
+                onChange={() => handleRatingChange(category)}
+              />
+            </label>
+          </div>
         ))}
       </div>
     </div>

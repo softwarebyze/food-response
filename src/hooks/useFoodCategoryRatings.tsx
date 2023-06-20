3b345355a2c1337075315853d0d88faf9f4e99ff
softwarebyze <zackebenfeld@gmail.com>
2023-06-20 04:27:02 -0400

    added FoodCategoryRatingData interface

--- src/hooks/useFoodCategoryRatings.tsx
+++ src/hooks/useFoodCategoryRatings.tsx
@@ -1,6 +1,15 @@
 import { useQuery } from '@tanstack/react-query'
 import { supabase } from '../supabaseClient'
 
+interface FoodCategoryRatingData {
+  id: number;
+  created_at: string;
+  user_id: string;
+  food_category: string;
+  rating: number;
+}
+
+
 const fetchFoodCategoryRatings = async () => {
   const { data: foodCategoryRatings, error } = await supabase
     .from('food_category_ratings')
@@ -10,7 +19,7 @@ const fetchFoodCategoryRatings = async () => {
     throw new Error(error.message)
   }
 
-  return foodCategoryRatings
+  return foodCategoryRatings as FoodCategoryRatingData[]
 }
 
 export function useFoodCategoryRatings() {

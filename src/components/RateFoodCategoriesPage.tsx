afa7a63471d980652d66ee8f9d47150c0ddd95fc
softwarebyze <zackebenfeld@gmail.com>
2023-06-20 02:24:31 -0400

    add submit button

--- src/components/RateFoodCategoriesPage.tsx
+++ src/components/RateFoodCategoriesPage.tsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import { allUnhealthyCategories } from '../data/images.ts'
+import useRateFoodCategories from '../hooks/useRateFoodCategories.tsx'
+import { Navigate } from 'react-router-dom'
 
 export default function RateFoodCategoriesPage() {
   const [currentFoodCategoryRatings, setCurrentFoodCategoryRatings] = useState(
@@ -26,23 +28,44 @@ export default function RateFoodCategoriesPage() {
     })
   }
 
+  const { mutate: rateFoodCategories, isSuccess } = useRateFoodCategories()
+  const handleSubmit = (e: any) => {
+    e.preventDefault()
+    rateFoodCategories(currentFoodCategoryRatings)
+  }
+
+  if (isSuccess) {
+    return <Navigate to="/ratefoods" />
+  }
+
   return (
     <div className="container">
       <h1 className="title">Choose Food Categories</h1>
       <h2 className="subtitle">Please choose at least 4 food categories</h2>
       <div className="column">
-        {currentFoodCategoryRatings.map(({ category, rating }) => (
-          <div key={category}>
-            <label>
-              {category}
-              <input
-                type="checkbox"
-                checked={Boolean(rating)}
-                onChange={() => handleRatingChange(category)}
-              />
-            </label>
-          </div>
-        ))}
+        <form>
+          {currentFoodCategoryRatings.map(({ category, rating }) => (
+            <div key={category}>
+              <label>
+                {category}
+                <input
+                  type="checkbox"
+                  checked={Boolean(rating)}
+                  onChange={() => handleRatingChange(category)}
+                />
+              </label>
+            </div>
+          ))}
+          <button
+            disabled={
+              currentFoodCategoryRatings.filter(({ rating }) => Boolean(rating))
+                .length < 4
+            }
+            onClick={handleSubmit}
+          >
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   )

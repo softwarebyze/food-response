dde905ea0574339cbdb37a385cbbac3777d68cb1
softwarebyze <zackebenfeld@gmail.com>
2023-06-19 22:22:56 -0400

    added content

--- src/components/RateFoodCategoriesPage.tsx
+++ src/components/RateFoodCategoriesPage.tsx
@@ -1,5 +1,9 @@
 export default function RateFoodCategoriesPage() {
- return (
-    <div>Rate Categories Route</div>
- )
+  return (
+    <div className="container">
+      <h1 className="title">Rate Food Categories Page</h1>
+      <h2 className="subtitle">Rated x food categories</h2>
+      <div className="columns is-centered"></div>
+    </div>
+  )
 }
